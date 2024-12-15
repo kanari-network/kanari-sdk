39f50bb25ae4862ad4b232cfdde7d720f7bc5170
jamesatomc <jiraphonwansai2002@gmail.com>
2024-12-16 01:04:39 +0700

    Refactor move_cli command handling and enhance command usage output

--- crates/command/src/move_cli/mod.rs
+++ crates/command/src/move_cli/mod.rs
@@ -1,141 +1,111 @@
 use colored::Colorize;
-use std::process::exit;
+use move_core_types::errmap::ErrorMapping;
+use move_vm_test_utils::gas_schedule::zero_cost_schedule;
+use move_package::BuildConfig;
+use std::{path::PathBuf, process::exit};
+use kari_move::{
+    base::{
+        build::Build,
+        coverage::{Coverage, CoverageSummaryOptions},
+        disassemble::Disassemble,
+        docgen::Docgen,
+        errmap::Errmap,
+        info::Info,
+        migrate::Migrate,
+        new::New,
+        test::Test,
+    }, run_cli, sandbox, Command, Move
+};
 
-pub fn handle_move_command() -> Option<String> {
-    // Collect command line arguments
+pub fn handle_move_command() {
     let args: Vec<String> = std::env::args().collect();
+    let cost_table = zero_cost_schedule();
+    let error_mapping = ErrorMapping::default();
+    
 
-    // Check if any arguments were provided
-    if args.len() > 2 {
-        // Collect command line arguments
-        let command = &args[2];
-        // Use string comparison in the match statement
-        match command.as_str() {
-            "build" => {
-                println!("Building package...");
-                return None;
-            }
-            "coverage" => {
-                // String comparison for "coverage"
-                println!("Inspecting test coverage...");
-                return None;
-            }
-            "disassemble" => {
-                // String comparison for "disassemble"
-                println!("Disassembling bytecode...");
-                return None;
-            }
-            "docgen" => {
-                // String comparison for "docgen"
-                println!("Generating documentation...");
-                return None;
-            }
-            "errmap" => {
-                // String comparison for "errmap"
-                println!("Generating error map...");
-                return None;
-            }
-            "info" => {
-                // String comparison for "info"
-                println!("Printing address information...");
-                return None;
-            }
-            "new" => {
-                // String comparison for "info"
-                println!("Printing address information...");
-                return None;                
-            }
-
-            "prove" => {
-                // String comparison for "prove"
-                println!("Running Move prover...");
-                return None;
-            }
-            "publish" => {
-                // String comparison for "publish"
-                println!("Publishing account...");
-                return None;
-            }
-            "run" => {
-                // String comparison for "run"
-                println!("Running Move function...");
-                return None;
-            }
-            "test" => {
-                // String comparison for "test"
-                println!("Running Move unit tests...");
-                return None;
-            }
-            "help" => {
-                // String comparison for "help"
-                println!("Printing help...");
-                return None;
-            }
-            _ => {
-                println!("{}", "Invalid command".red());
-                println!("Usage: kari move [OPTIONS] <COMMAND>");
-                println!("Commands:");
-                println!("  {} - Build the package at `path`. If no path is provided defaults to current directory", "build".green());
-                println!("  {} - Inspect test coverage for this package. A previous test run with the `--coverage` flag must have previously been run", "coverage".green());
-                println!(
-                    "  {} - Disassemble the Move bytecode pointed to",
-                    "disassemble".green()
-                );
-                println!(
-                    "  {} - Generate javadoc style documentation for Move packages",
-                    "docgen".green()
-                );
-                println!("  {} - Generate error map for the package and its dependencies at `path` for use by the Move explanation tool", "errmap".green());
-                println!("  {} - Print address information", "info".green());
-                println!(
-                    "  {} - CLI frontend for the Move compiler and VM",
-                    "new".green()
-                );
-                println!("  {} - Run the Move Prover on the package at `path`. If no path is provided defaults to current directory. Use `.. prove .. -- <options>` to pass on options to the prover", "prove".green());
-                println!(
-                    "  {} - Common options for interacting with an account for a validator",
-                    "publish".green()
-                );
-                println!("  {} - Run a Move function", "run".green());
-                println!("  {} - Run Move unit tests in this package", "test".green());
-                println!(
-                    "  {} - Print this message or the help of the given subcommand(s)",
-                    "help".green()
-                );
-                exit(1); // Exit with an error code
-            }
-        }
-    } else {
-        println!("{}", "Invalid command".red());
-        println!("Usage: kari move [OPTIONS] <COMMAND>");
-        println!("Commands:");
-        println!("  {} - Build the package at `path`. If no path is provided defaults to current directory", "build".green());
-        println!("  {} - Inspect test coverage for this package. A previous test run with the `--coverage` flag must have previously been run", "coverage".green());
-        println!(
-            "  {} - Disassemble the Move bytecode pointed to",
-            "disassemble".green()
-        );
-        println!(
-            "  {} - Generate javadoc style documentation for Move packages",
-            "docgen".green()
-        );
-        println!("  {} - Generate error map for the package and its dependencies at `path` for use by the Move explanation tool", "errmap".green());
+    if args.len() <= 2 {
+        println!("{}", "Usage: move <command> [options]".red());
+        println!("Available commands:");
+        println!("  {} - Build the package", "build".green());
+        println!("  {} - Inspect test coverage", "coverage".green());
+        println!("  {} - Disassemble Move bytecode", "disassemble".green());
+        println!("  {} - Generate documentation", "docgen".green());
+        println!("  {} - Generate error map", "errmap".green());
         println!("  {} - Print address information", "info".green());
-        println!(
-            "  {} - CLI frontend for the Move compiler and VM",
-            "new".green()
-        );
-        println!("  {} - Run the Move Prover on the package at `path`. If no path is provided defaults to current directory. Use `.. prove .. -- <options>` to pass on options to the prover", "prove".green());
-        println!(
-            "  {} - Common options for interacting with an account for a validator",
-            "publish".green()
-        );
-        println!("  {} - Run a Move function", "run".green());
-        println!("  {} - Run Move unit tests in this package", "test".green());
-        println!(
-            "  {} - Print this message or the help of the given subcommand(s)",
-            "help".green()
-        );
-        exit(1); // Exit with an error code
+        println!("  {} - Create new Move package", "new".green());
+        println!("  {} - Run Move unit tests", "test".green());
+        println!("  {} - Execute sandbox commands", "sandbox".green());
+        exit(1);
+    }
+
+    let command = &args[2];
+    let move_args = Move {
+        package_path: None,
+        verbose: false,
+        build_config: BuildConfig::default(),
+    };
+
+    let cmd = match command.as_str() {
+        "build" => Command::Build(Build {}),
+        // "coverage" => Command::Coverage(Coverage {
+        //     options: vec![
+        //         CoverageSummaryOptions::ModuleSummary,
+        //         CoverageSummaryOptions::FunctionSummary,
+        //         CoverageSummaryOptions::BytecodeSummary { module_name: None }
+        //     ]
+        // }),
+        "disassemble" => Command::Disassemble(Disassemble {
+            interactive: false,
+            package_name: None,
+            module_or_script_name: String::new(),
+            debug: false
+        }),
+        "docgen" => Command::Docgen(Docgen {
+            section_level_start: Some(0),
+            exclude_private_fun: false,
+            exclude_specs: false,
+            independent_specs: false,
+            exclude_impl: false,
+            toc_depth: Some(3),
+            no_collapsed_sections: false,
+            output_directory: None,
+            compile_relative_to_output_dir: false,
+            references_file: None,
+            template: Vec::new(),
+            include_dep_diagrams: false,
+            include_call_diagrams: false
+        }),
+        "errmap" => Command::Errmap(Errmap {
+            error_prefix: None,
+            output_file: PathBuf::new()
+        }),
+        "info" => Command::Info(Info {}),
+        "migrate" => Command::Migrate(Migrate {}),
+        "new" => Command::New(New {
+            name: args.get(3).map(String::from).unwrap_or_default()
+        }),
+        "test" => Command::Test(Test {
+            filter: None,
+            list: false,
+            num_threads: 1,
+            report_statistics: None,
+            check_stackless_vm: false,
+            verbose_mode: false,
+            compute_coverage: false,
+            gas_limit: None
+        }),
+        "sandbox" => Command::Sandbox {
+            storage_dir: PathBuf::from(kari_move::DEFAULT_STORAGE_DIR),
+            cmd: sandbox::cli::SandboxCommand::Clean {}
+        },
+        _ => {
+            println!("{}", "Invalid command".red());
+            exit(1);
+        }
+    };
+
+    if let Err(e) = run_cli(Vec::new(), &cost_table, &error_mapping, move_args, cmd) {
+        println!("{}: {}", "Error".red(), e);
+        exit(1);
     }
 }
