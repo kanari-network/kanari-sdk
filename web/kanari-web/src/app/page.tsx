e593538603f5e10fa3c1ed974d62f2006382662b
jamesatomc <jiraphonwansai2002@gmail.com>
2025-01-11 15:16:50 +0700

    Remove kanari-web submodule and associated configuration files

--- web/kanari-web/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import HeroSection from "./HeroSection";
-
-export default function Home() {
-  return (
-    <HeroSection />
-  );
-}
--- web/kanari-web/testPunycode.ts
+++
@@ -1,8 +0,0 @@
-const punycode = require('punycode/');
-
-// Example usage of punycode
-const encoded = punycode.encode('mañana');
-const decoded = punycode.decode(encoded);
-
-console.log(`Encoded: ${encoded}`);
-console.log(`Decoded: ${decoded}`);
