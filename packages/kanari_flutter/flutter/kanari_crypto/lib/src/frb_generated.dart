cf3cfa117134dc85cc91edd61b40a92dfb68dfbd
jamesatomc <jiraphonwansai2002@gmail.com>
2026-02-12 13:44:32 +0700

    chore: remove unused wasmBindgenName configuration
    This parameter is not used by the current Flutter Rust Bridge setup and its presence could cause confusion.

--- packages/kanari_flutter/flutter/kanari_crypto/lib/src/frb_generated.dart
+++ packages/kanari_flutter/flutter/kanari_crypto/lib/src/frb_generated.dart
@@ -71,7 +71,6 @@ class RustLib extends BaseEntrypoint<RustLibApi, RustLibApiImpl, RustLibWire> {
         stem: 'rust',
         ioDirectory: 'lib/src/',
         webPrefix: 'pkg/',
-        wasmBindgenName: 'wasm_bindgen',
       );
 }
 
